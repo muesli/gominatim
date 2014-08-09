6de9a38228f9d6de41d554544768cc5ff66403f5
grindhold <grindhold@gmx.net>
2014-08-10 01:14:58 +0200

    updated example

--- example/example.go
+++ example/example.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "github.com/grindhold/gominatim"
+	"fmt"
+	"github.com/grindhold/gominatim"
 )
 
 func main() {
-    gominatim.SetServer("http://nominatim.openstreetmap.org/search")
+	gominatim.SetServer("http://nominatim.openstreetmap.org/")
 
-    //Get by a Querystring
-    qry := new(gominatim.Query)
-    qry.Q = "Hamburg"
-    resp, _ := qry.Get() // Returns []gominatim.Result
-    fmt.Printf("Found location: %s (%s, %s)\n", resp[0].DisplayName, resp[0].Lat, resp[0].Lon)
+	//Get by a Querystring
+	qry := new(gominatim.SearchQuery)
+	qry.Q = "Hamburg"
+	resp, _ := qry.Get() // Returns []gominatim.Result
+	fmt.Printf("Found location: %s (%s, %s)\n", resp[0].DisplayName, resp[0].Lat, resp[0].Lon)
 
-    //Get by City
-    qry = &gominatim.Query{
-        City: "Berlin",
-    }
-    resp, _ = qry.Get()
-    fmt.Printf("Found location: %s (%s, %s)\n", resp[0].DisplayName, resp[0].Lat, resp[0].Lon)
+	//Get by City
+	qry = &gominatim.SearchQuery{
+		City: "Berlin",
+	}
+	resp, _ = qry.Get()
+	fmt.Printf("Found location: %s (%s, %s)\n", resp[0].DisplayName, resp[0].Lat, resp[0].Lon)
+
+	//Reverse Geocoding
+	rqry := new(gominatim.ReverseQuery)
+	rqry.Lat = "52.5170365"
+	rqry.Lon = "13.3888599"
+	rresp, _ := rqry.Get()
+	fmt.Printf("Found %s\n", rresp.DisplayName)
 }
