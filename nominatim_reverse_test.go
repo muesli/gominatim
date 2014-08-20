4fb7547bf154a61c56e7932cf3f72719abf8f036
grindhold <grindhold@gmx.net>
2014-08-20 20:44:56 +0200

    more tests for reverse search

--- nominatim_reverse_test.go
+++ nominatim_reverse_test.go
@@ -50,3 +50,54 @@ func Test_ReverseQueryWithoutServer(t *testing.T) {
 		t.Error("Expected error about unset server. Got none.")
 	}
 }
+
+func Test_OSMType(t *testing.T) {
+	defer SetServer("")
+	SetServer("http://nominatim.openstreetmap.org")
+	rqry := new(ReverseQuery)
+	rqry.Lat = "52.5170365"
+	rqry.Lon = "13.3888599"
+	rqry.OsmType = "V"
+	_, err := rqry.buildQuery()
+	if err != nil {
+		if !(err.Error() == "OsmType must be 'N', 'W' or 'R'") {
+			t.Error("Expecting error about Wrong OSMType. Received" + err.Error())
+		}
+	} else {
+		t.Error("Expecting error about Wrong OSMType. Got none.")
+	}
+
+	rqry = new(ReverseQuery)
+	rqry.Lat = "52.5170365"
+	rqry.Lon = "13.3888599"
+	_, err = rqry.buildQuery()
+	if err != nil {
+		t.Error("Expecting no error. Got " + err.Error())
+	}
+}
+
+func Test_LatLon(t *testing.T) {
+	defer SetServer("")
+	SetServer("http://nominatim.openstreetmap.org")
+	rqry := new(ReverseQuery)
+	rqry.Lon = "13.3888599"
+	_, err := rqry.buildQuery()
+	if err != nil {
+		if !(err.Error() == "Cannot search without a latitude. Set field Lat") {
+			t.Error("Expecting error about missing latitude. Received" + err.Error())
+		}
+	} else {
+		t.Error("Expecting error about missing latitude. Got none.")
+	}
+
+	rqry = new(ReverseQuery)
+	rqry.Lat = "52.5170365"
+	_, err = rqry.buildQuery()
+	if err != nil {
+		if !(err.Error() == "Cannot search without a longitude. Set field Lon") {
+			t.Error("Expecting error about missing longitude. Received" + err.Error())
+		}
+	} else {
+		t.Error("Expecting error about missing longitude. Got none.")
+	}
+}
